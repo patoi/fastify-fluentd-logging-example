54184331a7b909f068e549d56d07d3e3489fc304
István Pató <istvan.pato@gmail.com>
2018-09-29 14:27:20 +0200

    logging

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:carbon
+FROM node:10-alpine
 WORKDIR /usr/src/app
 COPY package*.json ./
 # we deliver tests and its dependencies
--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-// Require the framework and instantiate it
-const pino = require('pino')
-const fastify = require('fastify')({
-  logger: {
-    base: null,
-    timestamp: pino.stdTimeFunctions.unixTime
-  }
+const logger = require('pino')({
+  base: null,
+  useLevelLabels: true
 })
-
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  // saved to journalDB (but not to logDB)
-  fastify.log.info({dest: 'journal', msg:'journal message'})
-  // others saved to logDB
-  fastify.log.info({dest: 'log', msg:'log message'})
-  fastify.log.info({msg:'log object, but no dest key'})
-  fastify.log.info('string log message')
-  return { hello: 'world' }
-})
-
-// Run the server!
-const start = async () => {
-  try {
-    // you must specify 0.0.0.0
-    // with docker the 127.0.0.1 (default) is unreachable
-    await fastify.listen(4000, '0.0.0.0')
-    fastify.log.info({
-      dest: 'log',
-      msg: `server listening on ${fastify.server.address().port}`
-    })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+function logging () {
+  for (var i = 0; i < 1000; i++) {
+    logger.info({ 'msg': i })
   }
 }
-start()
+setTimeout(logging, 5000)
